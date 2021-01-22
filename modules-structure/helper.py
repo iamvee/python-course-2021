12c94372a0e2dab4ae82777de93dbcaf202b6459
iamvee <naeini.v@gmail.com>
2021-01-22 17:37:01 +0330

    module structure

---
+++ modules-structure/helper.py
@@ -0,0 +1,9 @@
+def echo(s):
+    return s
+
+
+value = 1000
+
+
+if __name__ == '__main__':
+    print("hello from helper")
---
+++ modules-structure/main.py
@@ -0,0 +1,7 @@
+import helper
+import my_directory
+import sys
+
+
+print(my_directory.whatever.val)
+
